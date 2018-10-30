56b27222cf189dfe7fbbdb12196a26732a71b97a
Roman Bulinski <aboandrus@gmail.com>
2018-10-30 22:39:37 +0100

    Permanent text save in local storage

--- main.js
+++ main.js
@@ -1,52 +1,107 @@
 
 
-
-document.body.addEventListener("load", loadInfoFromLocalStore() );
-
-
-
-
+window.addEventListener('load', loadInfoFromLocalStore );
 
 function loadInfoFromLocalStore(){
+  alert("LOAD");
+  // localStorage.clear();
+  
+  var len = localStorage.length;
 
-  for ( var i = 0, len = localStorage.length; i < len; ++i ) {
-    // console.log( localStorage.key( i ) + ": " + localStorage.getItem( localStorage.key( i ) ) );
-      
-      setTimeout(function(){ var idNot = create_note();}, 2000);
-      i=i+2;
-     
+  for ( var i = 0;  i < len; i++ ) {
+    for ( var j = i+1;  j < len; j++ ) {
+      for ( var k = j+1;  k < len; k++) {
+
+          var tem1 = localStorage.key( i ).split("_")[0];
+          // alert(tem1);
+          var tem2 = localStorage.key( j ).split("_")[0];
+          // alert(tem2);
+          var tem3 = localStorage.key( k ).split("_")[0];
+
+          
+          if( tem1==tem2 && tem2==tem3 ){
+            
+            var idNot = create_note();
+
+            var top =0;
+            if (localStorage.key(i).split("_")[1]=="top"){
+                top = localStorage.getItem(localStorage.key(i));
+            } else if (localStorage.key(j).split("_")[1]=="top"){
+                top = localStorage.getItem( localStorage.key(j) );
+            } else if (localStorage.key(k).split("_")[1]=="top"){
+                top = localStorage.getItem( localStorage.key(k));
+            }
+            console.log( "top : "+ top );
+           
+            var left =0;
+            if (localStorage.key(i).split("_")[1]=="left"){
+                left = localStorage.getItem( localStorage.key(i));
+            }else if(localStorage.key(j).split("_")[1]=="left"){
+                left = localStorage.getItem( localStorage.key(j));
+            }else if(localStorage.key(k).split("_")[1]=="left") {
+                left = localStorage.getItem( localStorage.key(k));
+            }    
+            console.log("left : " +left );
+            var textinnote ="";
+            if (localStorage.key(i).split("_")[1]=="textinnote"){
+                textinnote = localStorage.getItem( localStorage.key(i));
+            }else if(localStorage.key(j).split("_")[1]=="textinnote"){
+                textinnote = localStorage.getItem(localStorage.key(j));
+            }else if(localStorage.key(k).split("_")[1]=="textinnote"){
+                textinnote = localStorage.getItem(localStorage.key(k));
+            }
+            console.log( "text : "+ textinnote );
+
+            // console.log( document.getElementById(idNot).style[top] );
+
+            document.getElementById(idNot).style.top = top+"px";
+            document.getElementById(idNot).style.left = left+"px";
+            
+          
+            document.getElementById(idNot+"_textinnote").innerHTML=textinnote;
+          
+            i=i+2; 
+          }
+      }
+    } 
   }
 
-  console.log( " there is "+len/3+"notes"  )
+  console.log( " loadeds "+len/3+"notes"  )
 }
 
 
-function createNotesFromLocalStore(){
-  var idNot = create_note();
-
-
-  // document.getElementById("result").innerHTML = localStorage.getItem("lastname");
-
-}
 
 document.getElementById("createnote_button").addEventListener("click", create_note );
 
+
+
 function cancel_note(){
+
   var id_button = event.srcElement.id;
-  var len = id_button.length;
-  var id_card = id_button.substr(0, len - 3);
-  body.removeChild(document.getElementById(id_card));
+
+  var id_card = id_button.substring(0,id_button.length-3);
+
+  document.getElementById(id_card).remove();
+
+  localStorage.removeItem(id_card+"_textinnote");
+  localStorage.removeItem(id_card+"_top");
+  localStorage.removeItem(id_card+"_left");
+
 }
 
 function create_note() {
 
+  // generate unique id on basis time and random number from 0 -100
   var randomnumber = Math.floor(Math.random()*100);
   var time = new Date().getUTCMilliseconds();
-  var timestamp=time + randomnumber;
+  var timestamp=time +""+ randomnumber;
 
   note_frame = document.createElement("div");
   note_frame.setAttribute("id", timestamp);
-  note_frame.setAttribute("class", "frame_body");
+  note_frame.setAttribute("class", "framebody");
+
+  localStorage.setItem( timestamp+"_top", note_frame.offsetTop );
+  localStorage.setItem( timestamp+"_left", note_frame.offsetLeft);
   
   // header
       header = document.createElement("div");
@@ -62,10 +117,11 @@ function create_note() {
       cancel_button.addEventListener("click", cancel_note );
   // text area
       var text_area = document.createElement("TEXTAREA");
-      text_area.setAttribute("class", "text_innote");
-      text_area.setAttribute("id", timestamp+"text_innote");
+      text_area.setAttribute("class", "textinnote");
+      text_area.setAttribute("id", timestamp+"_textinnote");
       note_frame.appendChild(text_area);
-
+      text_area.addEventListener("keypress", putTextInHTML) 
+     
   document.body.appendChild(note_frame);
   // Make the DIV element draggable:
   dragElement(document.getElementById(timestamp));
@@ -120,12 +176,28 @@ function dragElement(elmnt) {
     var frame = document.getElementById(pureid);
 
     // document.getElementById(pureid+"text_innote").value = frame.offsetTop +" "+frame.offsetLeft;
-    var text = document.getElementById(pureid+"text_innote").value;
+    var text = document.getElementById(pureid+"_textinnote").value;
+    document.getElementById(pureid+"_textinnote").innerHTML=text;
 
-    // save information in local store memory
-    localStorage.setItem( pureid+"|top", frame.offsetTop );
-    localStorage.setItem( pureid+"|left", frame.offsetLeft);
-    localStorage.setItem( pureid+"|text", text); 
+    // save posytion in local store memory
+    localStorage.setItem( pureid+"_top", frame.offsetTop );
+    localStorage.setItem( pureid+"_left", frame.offsetLeft);
 
   }
 }
+
+function putTextInHTML(){
+  var id_textArea = event.srcElement.id;
+  var text = document.getElementById(id_textArea).value+"";
+  document.getElementById(id_textArea).innerHTML = text;
+  localStorage.setItem( id_textArea, text); 
+}
+
+
+
+
+
+
+
+
+
